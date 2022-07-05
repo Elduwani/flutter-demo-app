7e6c4add0a0d90053e119e72a0d938e84c725673
elduwani <elduwani@gmail.com>
2022-07-05 02:42:08 +0100

    wip: minesweeper

--- lib/pages/mine_sweeper.dart
+++ lib/pages/mine_sweeper.dart
@@ -1,7 +1,10 @@
 import 'package:demo_app/utils/mine_sweeper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:developer' as devtools show log;
+
+import '../components/input.dart';
 
 class MineSweeper extends StatefulWidget {
   const MineSweeper({Key? key}) : super(key: key);
@@ -86,77 +89,102 @@ class _MineSweeperState extends State<MineSweeper> {
     });
   }
 
+  Future signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 170,
-            color: Colors.grey[200],
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      bombCount.toString(),
-                      style: const TextStyle(fontSize: 40),
-                    ),
-                    const Text(
-                      "BOMBS",
-                      style: TextStyle(letterSpacing: _unit),
-                    ),
-                  ],
-                ),
-                GestureDetector(
-                  onTap: reset,
-                  child: Card(
-                    color: Colors.grey[700],
-                    child: const Icon(
-                      Icons.refresh,
-                      color: Colors.white,
-                      size: 40,
-                    ),
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          color: Colors.grey[200],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    bombCount.toString(),
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                  const Text(
+                    "BOMBS",
+                    style: TextStyle(letterSpacing: _unit),
+                  ),
+                ],
+              ),
+              GestureDetector(
+                onTap: reset,
+                child: Card(
+                  color: Colors.grey[700],
+                  child: const Icon(
+                    Icons.refresh,
+                    color: Colors.white,
+                    size: 40,
                   ),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text("0", style: TextStyle(fontSize: 40)),
-                    Text("TIME", style: TextStyle(letterSpacing: _unit)),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-          //Grid
-          Expanded(
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: gridCount,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: rowCount,
               ),
-              itemBuilder: (context, index) {
-                if (bombLocations.contains(index)) {
-                  return Bomb(
-                    revealed: squareStatus[index][1],
-                    function: detonate,
-                  );
-                }
-                return Box(
-                  text: squareStatus[index][0].toString(),
-                  revealed: squareStatus[index][1],
-                  function: () => revealBox(index),
-                );
-              },
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text("0", style: TextStyle(fontSize: 40)),
+                  Text("TIME", style: TextStyle(letterSpacing: _unit)),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        //Grid
+        Expanded(
+          child: GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: gridCount,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: rowCount,
             ),
-          )
-        ],
-      ),
+            itemBuilder: (context, index) {
+              if (bombLocations.contains(index)) {
+                return Bomb(
+                  revealed: squareStatus[index][1],
+                  function: detonate,
+                );
+              }
+              return Box(
+                text: squareStatus[index][0].toString(),
+                revealed: squareStatus[index][1],
+                function: () => revealBox(index),
+              );
+            },
+          ),
+        ),
+        const Center(
+          child: Text("Quantic Labs"),
+        ),
+        SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "${FirebaseAuth.instance.currentUser?.email}",
+                style: TextStyle(
+                  fontSize: _unit * 1.2,
+                  color: Colors.grey[600],
+                ),
+              ),
+              const SizedBox(height: _unit * 2),
+              Button(
+                function: signOut,
+                text: "Sign Out",
+                color: Colors.grey[900],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
